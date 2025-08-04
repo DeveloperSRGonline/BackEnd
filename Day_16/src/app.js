18f112e11d4815ff81256b060f3cd7b532bcce02
Developer SRG <shivamgarade05@gmail.com>
2025-08-04 09:33:07 +0000

    collage me kuchh code

--- Day_16/src/app.js
+++ Day_16/src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const authRoute = require("./routes/auth.route");
+const postRoute = require('./routes/post.route')
 const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json()); // middleware to read req.body
+
 app.use("/api/auth", authRoute);
+app.use("/api/post", postRoute);
 
 module.exports = app;
