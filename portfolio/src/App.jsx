ea8030f8995db0fac78cdcb627d4ceb43afa4c4f
Developer SRG <shivamgarade05@gmail.com>
2025-08-18 08:02:04 +0000

    .

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar"
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="hero">
+        <h1>I'm <span>Shivam</span><span> <img src="" alt="" /> </span></h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="nav">
+            <div className="logo">
+                <h3><i>Shivam</i></h3>
+            </div>
+            <div className="menu">
+                <img src="" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
